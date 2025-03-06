ff7858828b8b4fc939d743cc06a53172f3f56b00
kwnzax <kenzatt@outlook.com>
2025-03-06 13:47:02 +0100

    debut js+ login

---
+++ projet3/FrontEnd/script/login.js
@@ -0,0 +1,57 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('pw');
+
+
+// Ne JAMAIS faire confiance au front, donc vérifier le type email avec un regex, et vérifier le password pour le projet juste vérifier que le champs n'est pas vide
+
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+
+    const object = {
+        email: email.value,
+        password: password.value
+    }
+
+
+    console.log(object)
+
+    fetch('....', {
+        method: 'POST',
+        headers: { "Content-Type": "application/JSON" },
+        body: object
+    }).then(response => {
+        /*
+    
+        ... tout un tas d'information de l'api pas forcément utiles
+        */
+        // On vérifie si l'api retourne un OK (donc un status code 200)
+        if (response.ok) {
+            return response.json();
+        }
+    }).then(data => {
+
+
+        // Sauvegarder le token dans le localstorage
+
+        // Une fois le token sauvegarder on redirige l'utilisateur sur la page index.html
+
+        console.log(data);
+    }).catch(err => {
+        console.error(err);
+    })
+
+    /*
+    
+    {
+    
+    
+    email: '...',
+    password : ''
+    }
+    
+    */
+
+})
---
+++ projet3/FrontEnd/script/script.js
@@ -0,0 +1,6 @@
+// Vérifier l'existance ou non d'un TOKEN
+
+// SI token, je suis authentifié donc j'affiche le mode édition et le logout
+
+
+// Sinon, je cache le mode édition et le logout 
