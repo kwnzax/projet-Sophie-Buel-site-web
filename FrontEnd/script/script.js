328390b9c504f8bbb700ab77b2ce5831944d0add
kwnzax <kenzatt@outlook.com>
2025-03-20 17:27:11 +0100

    affichage des projets dynmique + filtres

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,3 +1,4 @@
+
 const token = localStorage.getItem('token');
 console.log(token);
 
@@ -6,23 +7,50 @@ console.log(token);
 // Dans le cas contraire j'affiche le bouton login et je cache le logout et le mode édition
 
 
-// Etape 1  : 
-
-// Créer les filtres de manière dynamique (Hotel, Objet ...) 
 function getFilters(){
-    // Faire un appel sur l'api /categories
-    // Avec la données retourner on créer les filtres donc une boucle sur chaque objet 
-    /*
+    
+    fetch('http://localhost:5678/api/categories')
+    
+    .then(response => {
+        return response.json()
+    }).then(categories => {
+        console.log(categories)
+        for(let i = 0; i < categories.length; i++) {
 
-    {
-    "id": 1,
-    "name": "Hotel",
-    
-    }, 
-    {
-    "id": 2,
-    "name": "Objet d'art",
-    }, 
-    
-    */
+            const sectionFilter = document.querySelector(".filters");
+            const btnFilter = document.createElement('button');
+            btnFilter.textContent = categories.name;
+
+            sectionFilter.appendChild(btnFilter)
+        }
+    })
 }
+
+
+getFilters();
+
+
+function genererProjets(){
+    fetch('http://localhost:5678/api/works')
+
+    .then(response => {
+        return response.json()
+    }).then(projets => {
+        for (let i = 0; i < projets.length; i++) {
+
+        const article = projets[i];
+        const sectionProjets = document.querySelector(".gallery");
+        const projetElement = document.createElement("figure"); 
+        const imageElement = document.createElement("img");
+        imageElement.src = article.imageUrl;
+        const captionElement = document.createElement("figcaption");
+        captionElement.innerText = article.title;
+
+        sectionProjets.appendChild(projetElement);
+        projetElement.appendChild(imageElement);
+        projetElement.appendChild(captionElement);
+        }
+    })
+}
+
+genererProjets();
