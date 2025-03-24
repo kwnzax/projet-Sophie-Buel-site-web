77508421eaa9d34deeea85db679e4120ec5eac2b
kwnzax <kenzatt@outlook.com>
2025-03-24 19:10:52 +0100

    chargement des works dynamique + correction modal

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -3,9 +3,11 @@ const token = localStorage.getItem('token');
 const selectCategory = document.getElementById('filters');
 const login = document.getElementById('login');
 const logout = document.getElementById('logout');
-const gallery  = document.getElementById('gallery');
+const gallery = document.getElementById('gallery');
 const edit = document.getElementById('edit')
 const editMode = document.getElementById('editMode')
+const btnFilter = document.getElementsByClassName('btn')
+
 
 logout.addEventListener('click', () => {
     localStorage.removeItem('token');
@@ -14,10 +16,14 @@ logout.addEventListener('click', () => {
 
 
 if (token) {
+    /*edit.forEach((element =>{
+        element.style.display = 'block'
+    }))*/
+    
     login.style.display = 'none';
     logout.style.display = 'block';
     edit.style.display = 'block';
-    editMode.style.display = 'block'
+    editMode.style.display = 'block';
 
 } else {
     logout.style.display = 'none';
@@ -35,62 +41,58 @@ function getFilters() {
             categories.forEach(category => {
                 const id = category.id;
                 const name = category.name
-                selectCategory.innerHTML += `<button value="${id}">${name}</button>`
+                selectCategory.innerHTML += `<button class="btn" value="${id}">${name}</button>`;
             })
+
+            for (const btn of btnFilter) {
+                btn.addEventListener('click', (e) => {
+                   
+                    const categoryID = e.target.value;
+                    getWorks(categoryID)
+                })
+            }
         })
 }
 
 
-function getWorks() {
+async function getWorks(categoryID = '0') {
     fetch('http://localhost:5678/api/works')
 
         .then(response => {
             return response.json()
         }).then(works => {
 
-            // Vider le dom 
-            gallery.innerHTML = '';
+       gallery.innerHTML = '';
 
-            for (let i = 0; i < works.length; i++) {
-
-                const article = works[i];
-                const sectionWorks = document.querySelector("#gallery");
-                const worksElement = document.createElement("figure");
-                const imageElement = document.createElement("img");
-                imageElement.src = article.imageUrl;
-                const captionElement = document.createElement("figcaption");
-                captionElement.innerText = article.title;
-
-                sectionWorks.appendChild(worksElement);
-                worksElement.appendChild(imageElement);
-                worksElement.appendChild(captionElement);
-            }
+       works
+       .filter(
+        (item) => 
+            item.categoryId.toString() === categoryID || categoryID === '0').forEach(works => {
+                gallery.innerHTML += `
+                <div class="work">
+                    <img src="${works.imageUrl}" alt="${works.title}">
+                    <h3>${works.title}</h3>
+                    <p>${works.description}</p>
+                </div>
+                `
+            })
 
             getWorksModal(works)
-
+       
         })
 }
 
-/*let btnFilter = document.querySelectorAll('.filters button')
-console.log(btnFilter)
-
-btnFilter.addEventListener('click', )
-function filters() {
-
-}*/
-
-
 const modal = document.querySelector('.modalBackground')
 
-edit.addEventListener('click', () => {
-
-     modal.style.display = 'flex';
+edit.addEventListener('click', (e) => {
+e.preventDefault();
+    modal.style.display = 'flex';
 })
 
 
-function getWorksModal(works){
-    console.log(works)
+function getWorksModal(works) {
+    // console.log(works)
 }
 
-getFilters();
 getWorks();
+getFilters();
